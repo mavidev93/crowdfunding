a1f549ad1bb8bece73cb5bf4d8b956d008398d05
unknown <mavidev93@gmail.com>
2022-04-24 11:41:41 +0430

    initial commit

--- .history/src/App_20220424113332.tsx
+++ .history/src/App_20220424113332.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
-import "./App.scss";
+import "./App.css";
 
 function App() {
   return (
